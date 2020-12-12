e5c32502ebb56fd895833e6c8f5efd3bc51ae64f
John Comeau <jc@unternet.net>
2020-12-11 16:12:46 -0800

    using newer toneDetectCfg from dist-ini

--- toneDetectCfg.py
+++ toneDetectCfg.py
@@ -5,7 +5,7 @@
 # $Author: emb $
 # String values should be quoted
 # Integers = int(10) Floats = float(0.1)
-# List's should be in () without quotes and seperated by commas bandPass = (250,30000)
+# List's should be in () without quotes and separated by commas bandPass = (250,30000)
 # The ini works by executing the command as written
 
 ########################################
@@ -13,21 +13,29 @@
 # fit your installation. Please read the
 # Documentation if you have any questions
 ########################################
+# If you don't like large logs change debug to info
 debugLevel = "debug"
+# Make sure you set this to your sound card
 deviceIndex = int(1)
-emailFrom = "dispatch-no-reply@jetcom.org"
-# Do not change lame info unless you are running in in a Mac or Linux
+# This only works if you are using an isp's smtp server
+emailFrom = "dispatch-no-reply@xxxxxx.org"
+# Don't change this if you are using the windows version. The windows
+# version installs lame in  the toneDetect directory
 localLame = "lame.exe"
 sendMail = bool(True)
-testEmail = "emb@jetcom.org"
+# Test mode will send emails for each tone sequence successfully decoded.
+# Unless you like getting a lot of email you should probably leave these
+# as they are.
+testEmail = "emb@xxxxxx.org"
 testMode = bool(False)
-smtp_server = "smtp.rochester.rr.com"
+# This is where you define your SMTP server.
+smtp_server = "smtp.xxxxxx.com"
 ## To use gmail instead of an local ISP provided SMTP server
 ## change the smtp_server as shown below and change the emailType
 ## to gmail. You will also need to add the appropriate user name
 ## and password
 #smtp_server = "smtp.gmail.com:587"
-# Current emailType are local or gmail
+# Current emailTypes are local or gmail
 emailType = "local"
 emailUser = ""
 emailPass = ""
@@ -40,13 +48,19 @@ emailPass = ""
 # any of these will change the way the program
 # works. Please read the Documentation  first.
 #########################################
-bandPass = (250,3000)
-maxSilenceLength = int(2)
-minDbLevel = int(45)
+# These numbers are a good place to start. I am working on some documentation
+# that will further explain them
+# Those that start with min or max are times in second, except minDBLevel
+bandPass = (250,3500) # Frequency in Hz for the bandpass filter
+maxSilenceLength = int(2) 
+minDbLevel = int(45)  # Minimum level (in dB) required to sense audio
 minGroupTime = float(4.0)
 minToneLength = float(0.6)
 minVoiceLength = int(10)
 sampleRate = int(11025)
 maxRecordLength = int(180)
+# for best performance the buffersize should be a power of 2. Changing it
+# will affect the ability of toneDetect to recognize some of the shorter 
+# plectron tones.
 bufferSize = int(1024)
 
